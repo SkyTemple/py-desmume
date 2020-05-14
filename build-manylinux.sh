36d648831f300bcdbd611df6f60f0f56469c137d
Parakoopa <parakoopa@live.de>
2020-05-14 03:34:53 +0200

    Mirror for pkg-config

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -9,7 +9,7 @@ rm /io/build -rf || true
 yum install zlib-devel libpcap-devel SDL-devel agg-devel soundtouch-devel openal-soft-devel glib2-devel libtool autoconf automake -y
 
 # Fun! The pkgconfig version of CentOS 6 is way too old!
-curl https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz -s -o /tmp/pkg-config.tar.gz
+curl https://parakoopa.de/pkg-config-0.29.2.tar.gz -s -o /tmp/pkg-config.tar.gz
 cd /tmp/
 tar -zxvf /tmp/pkg-config.tar.gz
 cd /tmp/pkg-config-0.29.2
