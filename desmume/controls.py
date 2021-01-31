09ab0bd05ab82e4bcfe4b4957897ca2f06410e3f
Parakoopa <parakoopa@live.de>
2021-01-31 23:02:21 +0100

    Localization support

--- desmume/controls.py
+++ desmume/controls.py
@@ -20,10 +20,14 @@ apply generally, such as the joystick configuration.
 # 
 #  You should have received a copy of the GNU General Public License
 #  along with py-desmume.  If not, see <https://www.gnu.org/licenses/>.
+import gettext
 from typing import Tuple, List, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from desmume.emulator import DeSmuME
+
+
+_ = gettext.gettext
 
 
 class Joy:
@@ -79,6 +83,25 @@ key_names = [
     "R", "L", "X", "Y",
     "Debug", "Boost",
     "Lid"
+]
+
+
+key_names_localized = [
+    _("A"),  # TRANSLATORS: DS Key name
+    _("B"),  # TRANSLATORS: DS Key name
+    _("Select"),  # TRANSLATORS: DS Key name
+    _("Start"),  # TRANSLATORS: DS Key name
+    _("Right"),  # TRANSLATORS: DS Key name
+    _("Left"),  # TRANSLATORS: DS Key name
+    _("Up"),  # TRANSLATORS: DS Key name
+    _("Down"),  # TRANSLATORS: DS Key name
+    _("R"),  # TRANSLATORS: DS Key name
+    _("L"),  # TRANSLATORS: DS Key name
+    _("X"),  # TRANSLATORS: DS Key name
+    _("Y"),  # TRANSLATORS: DS Key name
+    _("Debug"),  # TRANSLATORS: DS Key name
+    _("Boost"),  # TRANSLATORS: DS Key name
+    _("Lid")  # TRANSLATORS: DS Key name
 ]
 
 
