9893768864d5c9882725aa2c9d725e14f5accb74
Frostbyte <71291438+frostbyte0x70@users.noreply.github.com>
2025-12-12 03:55:10 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with py-desmume.  If not, see <https://www.gnu.org/licenses/>.
-__version__ = '0.0.7'
+__version__ = '0.0.9'
 from setuptools import setup, find_packages
 
 from setuptools.command.build_ext import build_ext
